951ca4a159a870a51330443ca6cd04173f712c5c
[HippieTaoist] <[shawn.m.kittel@gmail.com]>
2021-09-10 12:35:04 -0600

    File Complete

--- functions.js
+++ functions.js
@@ -35,14 +35,71 @@ const map = function (collection, iterator) {
   return result;
 };
 
-const filter = function (collection, callback) {};
+const filter = function (collection, callback) {
+  let resultArray = [];
+
+  each(collection, function (element, index, array) {
+    if (callback(element) === true) {
+      resultArray.push(element);
+    }
+
+  })
+  return resultArray;
+
+
+};
+
+
 //reject([1, 2, 3, 4, 5, 6], isEven);
-const reject = function (collection, callbackTest) {};
+const reject = function (collection, callbackTest) {
+  let resultArray = [];
+
+  each(collection, function (element, index, array) {
+    if (callbackTest(element) === false) {
+      resultArray.push(element);
+    }
+
+  })
+  return resultArray;
+
+
+};
 
 //use indexOf to solve this
-const uniq = function (array) {};
+const uniq = function (array) {
 
-const reduce = function (collection, iterator, accumulator) {};
+  let resultArray = []
+
+  each(array, function (element, index, list) {
+    console.log(indexOf(array, element), resultArray);
+    if (!resultArray.includes(element)) {
+      resultArray.push(element)
+    }
+  })
+  console.log(resultArray);
+  return resultArray;
+
+};
+
+const reduce = function (collection, iterator, accumulator) {
+
+  if (accumulator !== undefined) {
+
+    each(collection, function (element) {
+      accumulator = iterator(accumulator, element)
+
+    })
+    return accumulator;
+  } else {
+
+    let total = +collection.splice(0, 1)
+    each(collection, function (element) {
+      total = iterator(total, element)
+    })
+    return total;
+  }
+
+};
 
 module.exports = {
   filter,
